0197cb448deda7c6710c739b76f2757167966ab3
Antonio <antoniooficial2004@gmail.com>
2023-06-06 10:55:48 +0200

    script: set theme changes and save the actual theme in localStorage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
+const body = document.body;
 const menu = document.querySelector("#menu");
+const changeTheme = document.querySelector("#changeTheme");
+const btn_span = changeTheme.querySelector("span");
+const form = document.querySelector("form");
 
-menu.addEventListener("click", ()=>{
-    menu.classList.toggle("active");
+function themeSetted(){
+    if(body.classList.contains("light-theme")){
+        btn_span.textContent = "Light";
+        localStorage.setItem("theme", "light-theme");
+    }
+    else if(body.classList.contains("dark-theme")){
+        btn_span.textContent = "Dark";
+        localStorage.setItem("theme", "dark-theme");
+    }
+}
+
+if(localStorage.getItem("theme")){
+    body.classList.add(localStorage.getItem("theme"));
+}
+else{
+    body.classList.add("light-theme");
+}
+
+menu.addEventListener("click", ()=> menu.classList.toggle("active") );
+
+changeTheme.addEventListener("click", ()=>{
+    body.classList.toggle("dark-theme");
+    body.classList.toggle("light-theme");
+
+    themeSetted();
+
+    localStorage.setItem("theme", body.classList[0]);
 });
+
+form.addEventListener("submit", event => event.preventDefault() );
